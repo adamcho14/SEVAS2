b9fb27e110a13151f83353cce42b61d184286532
adamcho14 <adamcho14@gmail.com>
2018-04-22 12:51:30 +0200

    Attempt to use cookies and S/MIME encryption

---
+++ local/key_management.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#codin: utf-8
+#https://cryptography.io/en/latest/
+
+from secretsharing import SecretSharer
+
+key = 0
+
+
+shares
--- login.py
+++ login.py
@@ -3,10 +3,20 @@
 
 #This is to be provided by Cosign, but for now...
 
-print """Content-type: text/html
+from os import environ
+import cgi, cgitb
 
-<form method="post" action="voting.py">
-<input type="text" name="login" placeholder="Log in" required>
-<br/>
-<input type="submit" value="Submit">
-</form>"""
+if environ.has_key('HTTP_COOKIE'):
+    for cookie in map(environ['HTTP_COOKIE'].strip(), environ['HTTP_COOKIE'].split(';')):
+        (key, value) = cookie.split('=')
+        if key == "REMOTE_USER":
+            login = value
+
+else:
+    print """Content-type: text/html
+
+    <form method="post" action="voting.py">
+    <input type="text" name="login" placeholder="Log in" required>
+    <br/>
+    <input type="submit" value="Submit">
+    </form>"""
